c41dde9cb69fb6caf0f48f3d358583464d0d0cf9
Fernando Lopes da Silva <fernandolopes.s@gmail.com>
2015-03-20 18:50:27 -0300

    Terminado mais um tutorial com Flex e Spring

--- src/flex/HelloWorldService.java
+++ src/flex/HelloWorldService.java
@@ -2,12 +2,15 @@ package flex;
 
 import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.flex.remoting.RemotingInclude;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 @Service
 @RemotingDestination
 public class HelloWorldService {
+	
 	@RemotingInclude
+	@Secured("ROLE_USER")
 	public String sayHello(String name){
 		return "howdy, " + name;
 	}
